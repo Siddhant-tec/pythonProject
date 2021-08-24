18f41e4131b7f298a4da33a8243731c346f44b26
Siddhant-tec <siddhanttiwari200@gmail.com>
2021-08-24 21:12:35 +0530

    tcs_nqt_Decode_Encode.py rebuilt using classes and objects

--- tcs_nqt_Decode_Encode.py
+++ tcs_nqt_Decode_Encode.py
@@ -1,15 +1,23 @@
+class Encrypt:
+    def __init__(self, encode, decode):
+        self.encode = encode
+        self.decode = decode
+
+    def encoder(self):
+        encoded = list()
+        for values in encode:
+            encoded.append(chr((ord(values)) + 5))
+        print(''.join(encoded))
+
+    def decoder(self):
+        decoded = list()
+        for values2 in decode:
+            decoded.append(chr((ord(values2)) - 5))
+        print(''.join(decoded))
+
+
 encode = input()
 decode = input()
-
-encoded = list()
-decoded = list()
-
-for values in encode:
-    encoded.append(chr((ord(values))+5))
-
-for values2 in decode:
-    decoded.append(chr((ord(values2))-5))
-
-
-print(''.join(encoded))
-print(''.join(decoded))
+enc = Encrypt(encode, decode)
+enc.encoder()
+enc.decoder()
