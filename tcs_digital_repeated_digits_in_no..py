a2bdc29031c8fef8bad179962aa35ee2de584900
Siddhant-tec <siddhanttiwari200@gmail.com>
2021-08-25 23:06:31 +0530

    Added 3 digit functionality in  tcs_digital_repeated_digits_in_no..py.py (my tcs digital exam question)

--- tcs_digital_repeated_digits_in_no..py
+++ tcs_digital_repeated_digits_in_no..py
@@ -5,10 +5,16 @@ nums = list(map(str, range(start, end+1)))
 
 def has_doubles(nums):
     for number in nums:
-        if number[0] == number[1]:
-            print(number)
-        else:
-            continue
+        if len(number) == 2:
+            if number[0] == number[1]:
+                print(number)
+            else:
+                continue
+        if len(number) == 3:
+            if number[0] == number[1] == number[2]:
+                print(number)
+            else:
+                continue
 
 
 has_doubles(nums)
