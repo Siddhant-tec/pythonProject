e768082f365244c96d626447da0b09cc725c61d0
Siddhant-tec <siddhanttiwari200@gmail.com>
2021-08-25 23:17:18 +0530

    Improved the Added 3 digit functionality in  tcs_digital_repeated_digits_in_no..py.py (my tcs digital exam question).

--- tcs_digital_repeated_digits_in_no..py
+++ tcs_digital_repeated_digits_in_no..py
@@ -4,19 +4,25 @@ nums = list(map(str, range(start, end+1)))
 
 
 def has_doubles(nums):
+    count = 0
     for number in nums:
         if len(number) == 2:
             if number[0] == number[1]:
                 print(number)
+                count += 1
             else:
                 continue
         if len(number) == 3:
             if number[0] == number[1] == number[2]:
                 print(number)
+                count += 1
             elif number[0] == number[1] or number[1] == number[2] or number[2] == number[1]:
                 print(number)
+                count += 1
             else:
                 continue
+    print("No of repeated digits is: ", count)
+    print("No of non-repeated digits is: ", len(nums) - count)
 
 
 has_doubles(nums)
