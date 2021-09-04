081c96000df2be02962bf73fc88f89431fcb9716
Siddhant-tec <siddhanttiwari200@gmail.com>
2021-09-04 23:45:42 +0530

    added addition

--- 3input_list.py
+++ 3input_list.py
@@ -6,3 +6,11 @@ for i in range(rows):
 print(matrix)
 for element in matrix[0]:
     print(element)
+
+x = [sum(x) for x in zip(*matrix)]
+print(x)
+
+matrix2 = [[3, 4], [5, 6]]
+result = [list(map(sum, zip(*t))) for t in zip(matrix, matrix2)]
+print(result)
+print(matrix + matrix2)
---
+++ add_2_matrix.py
@@ -0,0 +1,11 @@
+X = [[1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9]]
+
+Y = [[9, 8, 7],
+     [6, 5, 4],
+     [3, 2, 1]]
+
+result = [list(map(sum, zip(*t))) for t in zip(X, Y)]
+
+print(result)
