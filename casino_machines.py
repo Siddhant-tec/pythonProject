9c96ca4545ff938689da271ee721877bdf024c15
Siddhant-tec <siddhanttiwari200@gmail.com>
2022-05-18 10:07:46 +0530

    added various programs as part of LTI training

---
+++ casino_machines.py
@@ -0,0 +1,29 @@
+rubles = int(input())
+A = int(input())
+B = 30
+C = 9
+count = 0
+
+while rubles > 0:
+    A += 1
+    rubles -= 1
+    count += 1
+    if A == 25:
+        rubles += 20
+        A = 0
+
+    B += 1
+    rubles -= 1
+    count += 1
+    if B == 120:
+        rubles += 80
+        B = 0
+
+    C += 1
+    rubles -= 1
+    count += 1
+    if C == 12:
+        rubles += 8
+        C = 0
+
+print(count - 1)
